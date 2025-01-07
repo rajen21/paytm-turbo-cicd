934bab793958c716dce1ea0bc286d17c938dbe48
Rajan chavda <www.chavdarajan@gmail.com>
2025-01-07 23:55:15 +0530

    test

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -12,6 +12,8 @@ declare global {
 
 const prisma: ReturnType<typeof prismaClientSingleton> = globalThis.prismaGlobal ?? prismaClientSingleton()
 
+
+
 export default prisma
 
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
